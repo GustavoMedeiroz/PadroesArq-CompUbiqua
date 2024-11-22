171d41777a91543b98ca8e8c66109c5f61ebc86c
WendelFl <wendelfl.dev@gmail.com>
2024-11-21 21:21:28 -0300

    :construction: feat: cors in progress

--- spectre-api/src/main/java/com/spectre/Spectre/infrastructure/configuration/BeanProvider.java
+++ spectre-api/src/main/java/com/spectre/Spectre/infrastructure/configuration/BeanProvider.java
@@ -1,4 +1,49 @@
 package com.spectre.Spectre.infrastructure.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
 public class BeanProvider {
+
+    @Bean(name = "corsConfiguration")
+    public CorsConfigurationSource corsConfiguration() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.setAllowedHeaders(allowedHeaders());
+        corsConfiguration.setAllowedMethods(allowedMethods());
+        corsConfiguration.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
+    }
+
+    private static List<String> allowedHeaders() {
+        return List.of(
+                "Bearer ",
+                "Authorization",
+                "Content-Type",
+                "Access-Control-Allow-Credentials",
+                "Access-Control-Allow-Headers"
+        );
+    }
+
+    private static List<String> allowedMethods() {
+        return List.of(
+                HttpMethod.GET.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.DELETE.name(),
+                HttpMethod.PATCH.name(),
+                HttpMethod.OPTIONS.name()
+        );
+    }
 }
